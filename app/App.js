4dd34e1ed9f4fc2f1ac07baecaa3b2866ab366a0
moad7 <moadpc77@gmail.com>
2022-11-07 13:54:17 +0200

    8112022

--- app/App.js
+++ app/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View,Text,StyleSheet,TouchableOpacity,Alert } from "react-native";  
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App=()=>{
+
+
+    const doSomething =async()=>{
+       const api='https://itunes.apple.com/search?term=metallica'
+       const response= await fetch(api,{
+        method:'get'
+       });
+
+       const data = await response.json()
+       console.log(data)
+    }
+
+    return(
+        <View style={myStyle.container}>
+            <TouchableOpacity style={myStyle.btn} onPress={doSomething}>
+                <Text style={myStyle.btntext}>Click Me</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
+const myStyle=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'#DEE5E5',
+        padding:30
+    },
+    btn:{
+        width:'100%',
+        paddingVertical:22,
+        alignItems:'center',
+        backgroundColor:'#17B890',
+        borderRadius:15
+    },
+    btntext:{
+        color:'#DEE5E5',
+        fontSize:22,
+        fontWeight:'700'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    }
+})
+
+
+export default App;
